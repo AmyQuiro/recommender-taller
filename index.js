1b7f5b1000e9686dc7527ecdb96d3bf69e7cade2
DiegoHerbas <dherbas@grupopaz.com.bo>
2023-01-26 00:33:29 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Initialize infrastructure
+require("./Infra/server/app");
+//Initialize db
+// import './Infra/database/sequelize';
+//# sourceMappingURL=index.js.map
